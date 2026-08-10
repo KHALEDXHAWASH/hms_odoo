e6772bba03161a6d6d2731138af89e510569ba2a
KHALEDXHAWASH <khaledhawash143@gmail.com>
2026-08-10 14:36:07 +0300

    fixed a typo

--- models/hms_log.py
+++ models/hms_log.py
@@ -4,5 +4,5 @@ class HMSLog(models.Model):
     _description = 'HMS Log'
     patient_id = fields.Many2one('hms.patient',string="Patient")
     created_at = fields.Datetime(string="Created at",related="patient_id.create_date")
-    created_by = fields.Many2one("res.users",string="Created bu",related="patient_id.create_uid")
+    created_by = fields.Many2one("res.users",string="Created by",related="patient_id.create_uid")
     history=fields.Html(string="Description")
